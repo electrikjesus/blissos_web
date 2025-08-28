60d8c0e19bcf72089e99839dc0c23c921dd86ac1
Onlook <git@onlook.com>
2025-08-28 18:50:10 -0400

    Update server.js to load downloads data
    Change complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3010;
@@ -11,11 +12,26 @@ app.set("views", path.join(__dirname, "views"));
 // Serve static files
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 
+// Load data files
+function loadDownloadsData() {
+  try {
+    const downloadsPath = path.join(__dirname, "data", "downloads.json");
+    const downloadsData = fs.readFileSync(downloadsPath, "utf8");
+    return JSON.parse(downloadsData);
+  } catch (error) {
+    console.error("Error loading downloads data:", error);
+    return { builds: [], metadata: {} };
+  }
+}
+
 // Basic routes
 app.get("/", (req, res) => {
+  const downloadsData = loadDownloadsData();
+
   res.render("index", {
     title: "BlissOS",
     description: "Android for your PC",
+    downloads: downloadsData,
   });
 });
 
